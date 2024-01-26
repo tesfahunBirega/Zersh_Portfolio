5c9af81e25ec010df677bab711bb6020872f1071
Nahom Debele <nahomdebele002@gmail.com>
2024-01-26 23:54:09 +0300

    added the redux slices

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -6,31 +6,7 @@ function Blogs() {
   const dispatch = useDispatch();
   const blogs = useSelector((state) => state.blogs.blogs);
   console.log(blogs, "blog");
-  const blogData = [
-    {
-      id: "1",
-      author: "Author 1",
-      title: "Blog 1",
-      description:
-        "This is the first blog post. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      imageUrl: "https://placekitten.com/400/300",
-      body: "bodybod",
-      category: "Technology",
-      createdDate: "January 10, 2022",
-    },
-    {
-      id: "2",
-      author: "Author 1",
-      title: "Blog 2",
-      description:
-        "This is the second blog post. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      body: "bodybod",
 
-      imageUrl: "https://placekitten.com/401/300",
-      category: "Travel",
-      createdDate: "February 15, 2022",
-    },
-  ];
   return (
     <div className="relative z-0 bg-primary h-screen overflow-y-scroll scroll-smooth">
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center h-1/2 px-20">
