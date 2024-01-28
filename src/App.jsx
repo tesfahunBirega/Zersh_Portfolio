37ba7b67d19fe09fe9cbf00a513b73d6f68da6a6
Nahom Debele <nahomdebele002@gmail.com>
2024-01-28 22:46:44 +0300

    added the login and singup routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ProjectDetailPage } from "./components";
 import Home from "./Pages/Home";
 import Blogs from "./Pages/Blogs";
 import SingleBlog from "./Pages/SngleBlog";
+import Signup from "./components/Auth/Signup";
 
 const App = () => {
   return (
@@ -15,6 +16,8 @@ const App = () => {
         <Route path="/projectdetail/:name" element={<ProjectDetailPage />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/blogs/:id" element={<SingleBlog />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
