856d782153ff7fba96a38f39d6e33957a62a1f45
Nahom Debele <nahomdebele002@gmail.com>
2024-01-30 22:47:29 +0300

    added okr state

---
+++ src/store/okr/okrSlice.js
@@ -0,0 +1,61 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchOkr, fetchOkrs } from "./okrAction";
+
+export const getOkrSlice = createSlice({
+    name:"okr",
+    initialState:{
+        goals:[],
+        yearly:[],
+        quarterly:[],
+        monthly:[],
+        weekly:[],
+        daily:[],
+
+        loading:false,
+        error:null
+    },
+    reducers:{},
+    extraReducers:(builder)=>{
+        builder.addCase(fetchOkr.pending,(state)=>{
+            state.loading = true
+        })
+        .addCase(fetchOkr.fulfilled, (state,action)=>{
+            state.loading = false
+            state.blog=action.payload
+        })
+        .addCase(fetchOkr.rejected , (state , action )=>{
+            state.loading = false
+            state.error = action.error.message
+        })
+    }
+})
+
+export const getOkrsSlice = createSlice({
+    name:'okrs',
+    initialState:{
+        goals:[],
+        yearly:[],
+        quarterly:[],
+        monthly:[],
+        weekly:[],
+        daily:[],
+        loading:false,
+        error:null
+    },
+    reducers:{},
+    extraReducers:(builder)=>{
+        builder
+            .addCase(fetchOkrs.pending , (state)=>{
+                state.loading = true
+            })
+            .addCase(fetchOkrs.fulfilled, (state,action)=>{
+                state.loading = false
+                state.blogs = action.payload
+            })
+            .addCase(fetchOkrs.rejected , (state, action)=>{
+                state.loading = false 
+                state.error = action.error.message
+            })
+    }
+})
+
