aadf89b00d78a5f7374e6f411bfdd51d0f37b22d
Nahom Debele <nahomdebele002@gmail.com>
2024-01-31 21:47:15 +0300

    changes the root reducers

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,9 +1,14 @@
 import { combineReducers } from "redux";
 import { blogSlice, blogsSlice } from "./blog/blogSlice";
-
+import { getNoteSlice, getNotesSlice } from "./note/noteSlice";
+import { getOkrSlice, getOkrsSlice } from "./okr/okrSlice";
 const rootReducer = combineReducers({
-    blog: blogSlice.reducer,
-    blogs: blogsSlice.reducer,
-  });
+  blog: blogSlice.reducer,
+  blogs: blogsSlice.reducer,
+  note:getNoteSlice.reducer,
+  notes:getNotesSlice.reducer,
+  okr:  getOkrSlice.reducer,
+  okrs:getOkrsSlice.reducer
+});
 
 export default rootReducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,5 @@
 import { combineReducers, configureStore, createListenerMiddleware } from "@reduxjs/toolkit"
-import { blogSlice, blogsSlice } from "./blog/blogSlice";
-import { getNoteSlice, getNotesSlice } from "./note/noteSlice";
-import { getOkrSlice, getOkrsSlice } from "./okr/okrSlice";
-
-const rootReducer = combineReducers({
-  blog: blogSlice.reducer,
-  blogs: blogsSlice.reducer,
-  note:getNoteSlice.reducer,
-  notes:getNotesSlice.reducer,
-  okr:  getOkrSlice.reducer,
-  okrs:getOkrsSlice.reducer
-});
+import rootReducer from "./rootReducer";
 
 const listenerMiddlewareInstance = createListenerMiddleware({
   onError: () => console.error,
