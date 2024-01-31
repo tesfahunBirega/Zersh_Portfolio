7165ff0c9d7d819ba26e90617847f5f544467cb7
Nahom Debele <nahomdebele002@gmail.com>
2024-01-31 21:42:01 +0300

    added the note and okr reducer to root reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,32 +1,18 @@
-// import { createStore, applyMiddleware } from "redux";
-// import thunk from "redux-thunk";
-// import { composeWithDevTools } from "redux-devtools-extension";
-// import rootReducer from "./rootReducer";
-
 import { combineReducers, configureStore, createListenerMiddleware } from "@reduxjs/toolkit"
 import { blogSlice, blogsSlice } from "./blog/blogSlice";
-
-// const initialState = {};
-// const middleware = [thunk];
-
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   composeWithDevTools(applyMiddleware(...middleware))
-// );
-// export default store;
+import { getNoteSlice, getNotesSlice } from "./note/noteSlice";
 
 const rootReducer = combineReducers({
   blog: blogSlice.reducer,
   blogs: blogsSlice.reducer,
+  note:getNoteSlice.reducer,
+  notes:getNotesSlice.reducer
 });
 
-// Instantiate the listener middleware
 const listenerMiddlewareInstance = createListenerMiddleware({
   onError: () => console.error,
 });
 
-// Configure the store with the rootReducer and middleware
 const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
