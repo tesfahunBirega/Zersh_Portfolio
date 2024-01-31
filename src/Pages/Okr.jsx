f038b17749da9baa4f5102a881d1ac81c5f83a5e
Nahom Debele <nahomdebele002@gmail.com>
2024-01-31 22:09:19 +0300

    added the dashboard

--- src/Pages/Okr.jsx
+++ src/Pages/Okr.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Button, Table, Modal, Form, Input } from "antd";
+import Dashboard from "../commons/Dashboard";
 
 const { Column } = Table;
 
@@ -35,49 +36,55 @@ const Okr = () => {
   };
 
   return (
-    <div className="container mx-auto mt-8">
-      <h1 className="text-3xl font-bold mb-4">Goals Management</h1>
-      <Button type="primary" onClick={showModal} className=" mb-4">
-        Add Goal
-      </Button>
-      <Modal
-        title="Add Goal"
-        open={isModalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <Form form={form} layout="vertical">
-          <Form.Item
-            name="name"
-            label="Goal Name"
-            rules={[{ required: true, message: "Please enter goal name" }]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item
-            name="description"
-            label="Description"
-            rules={[{ required: true, message: "Please enter description" }]}
-          >
-            <Input.TextArea />
-          </Form.Item>
-        </Form>
-      </Modal>
-      <Table dataSource={goals} rowKey="_id">
-        <Column title="Name" dataIndex="name" key="name" />
-        <Column title="Description" dataIndex="description" key="description" />
-        {/* Add more columns for other fields */}
-        <Column
-          title="Action"
-          key="action"
-          render={(text, record) => (
-            <Button type="danger" onClick={() => handleDelete(record._id)}>
-              Delete
-            </Button>
-          )}
-        />
-      </Table>
-    </div>
+    <Dashboard>
+      <div className="container mx-auto mt-8">
+        <h1 className="text-3xl font-bold mb-4">Goals Management</h1>
+        <Button type="primary" onClick={showModal} className=" mb-4">
+          Add Goal
+        </Button>
+        <Modal
+          title="Add Goal"
+          open={isModalVisible}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+          <Form form={form} layout="vertical">
+            <Form.Item
+              name="name"
+              label="Goal Name"
+              rules={[{ required: true, message: "Please enter goal name" }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              name="description"
+              label="Description"
+              rules={[{ required: true, message: "Please enter description" }]}
+            >
+              <Input.TextArea />
+            </Form.Item>
+          </Form>
+        </Modal>
+        <Table dataSource={goals} rowKey="_id">
+          <Column title="Name" dataIndex="name" key="name" />
+          <Column
+            title="Description"
+            dataIndex="description"
+            key="description"
+          />
+          {/* Add more columns for other fields */}
+          <Column
+            title="Action"
+            key="action"
+            render={(text, record) => (
+              <Button type="danger" onClick={() => handleDelete(record._id)}>
+                Delete
+              </Button>
+            )}
+          />
+        </Table>
+      </div>
+    </Dashboard>
   );
 };
 
