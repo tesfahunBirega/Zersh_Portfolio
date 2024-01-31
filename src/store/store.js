5eda1f5bf848007585d72fc8381390920c454334
Nahom Debele <nahomdebele002@gmail.com>
2024-01-31 21:43:19 +0300

    added the note and okr reducer to root reducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,15 @@
 import { combineReducers, configureStore, createListenerMiddleware } from "@reduxjs/toolkit"
 import { blogSlice, blogsSlice } from "./blog/blogSlice";
 import { getNoteSlice, getNotesSlice } from "./note/noteSlice";
+import { getOkrSlice, getOkrsSlice } from "./okr/okrSlice";
 
 const rootReducer = combineReducers({
   blog: blogSlice.reducer,
   blogs: blogsSlice.reducer,
   note:getNoteSlice.reducer,
-  notes:getNotesSlice.reducer
+  notes:getNotesSlice.reducer,
+  okr:  getOkrSlice.reducer,
+  okrs:getOkrsSlice.reducer
 });
 
 const listenerMiddlewareInstance = createListenerMiddleware({
