3b21fe71e7096e29c71d76bc281917d1d01c0a91
Nahom Debele <nahomdebele002@gmail.com>
2024-02-01 23:43:50 +0300

    note pagination have errors

--- src/Pages/Note.jsx
+++ src/Pages/Note.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Dashboard from "../commons/Dashboard";
 import NoteForm from "../components/NoteComponent/NoteForm";
 import NoteList from "../components/NoteComponent/NoteList";
-import { Button } from "antd";
+import { Button, Pagination } from "antd";
 import CategoryTabs from "../components/NoteComponent/CategoryTabs";
 import NoteCard from "../components/NoteComponent/NoteCard";
 
@@ -36,21 +36,109 @@ function Note() {
       color: "#ccffff",
       category: "Personal",
     },
+    {
+      id: 4,
+      title: "Note 4",
+      content: "Content of Note 4",
+      color: "#ccffff",
+      category: "Personal",
+    },
+    {
+      id: 4,
+      title: "Note 4",
+      content: "Content of Note 4",
+      color: "#ccffff",
+      category: "Personal",
+    },
+    {
+      id: 4,
+      title: "Note 4",
+      content: "Content of Note 4",
+      color: "#ccffff",
+      category: "Personal",
+    },
+    {
+      id: 4,
+      title: "Note 4",
+      content: "Content of Note 4",
+      color: "#ccffff",
+      category: "Personal",
+    },
+    {
+      id: 4,
+      title: "Note 4",
+      content: "Content of Note 4",
+      color: "#ccffff",
+      category: "Personal",
+    },
+    {
+      id: 4,
+      title: "Note 4",
+      content: "Content of Note 4",
+      color: "#ccffff",
+      category: "Personal",
+    },
+    {
+      id: 4,
+      title: "Note 4",
+      content: "Content of Note 4",
+      color: "#ccffff",
+      category: "Personal",
+    },
+    {
+      id: 4,
+      title: "Note 4",
+      content: "Content of Note 4",
+      color: "#ccffff",
+      category: "Personal",
+    },
+    {
+      id: 4,
+      title: "Note 4",
+      content: "Content of Note 4",
+      color: "#ccffff",
+      category: "Personal",
+    },
+    {
+      id: 4,
+      title: "Note 4",
+      content: "Content of Note 4",
+      color: "#ccffff",
+      category: "Personal",
+    },
+    {
+      id: 4,
+      title: "Note 4",
+      content: "Content of Note 4",
+      color: "#ccffff",
+      category: "Personal",
+    },
   ]);
+  const pageSize = 6;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
   const categories = ["All", "Personal", "Work", "Ideas"];
 
   const [selectedCategory, setSelectedCategory] = useState("All");
 
+  const handleSelectCategory = (category) => {
+    setSelectedCategory(category);
+    setCurrentPage(1);
+  };
   const filteredNotes =
     selectedCategory === "All"
       ? notes
       : notes.filter((note) => note.category === selectedCategory);
 
-  const handleSelectCategory = (category) => {
-    setSelectedCategory(category);
-  };
   const [openAddNote, setAddNote] = useState(false);
-
+  const paginatedNotes = filteredNotes.slice(
+    (currentPage - 1) * pageSize,
+    currentPage * pageSize
+  );
   const addNote = (note) => {
     setNotes([...notes, note]);
   };
@@ -86,10 +174,17 @@ function Note() {
           setVisble={setAddNote}
         />
         <div className="flex flex-wrap">
-          {filteredNotes.map((note) => (
+          {paginatedNotes.map((note) => (
             <NoteCard deleteNote={deleteNote} key={note.id} note={note} />
           ))}
         </div>
+        <Pagination
+          className="mt-4"
+          current={currentPage}
+          total={filteredNotes.length}
+          pageSize={pageSize}
+          onChange={handlePageChange}
+        />
       </div>
     </Dashboard>
   );
