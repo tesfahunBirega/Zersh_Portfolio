2667d84758e2a7639096c45300f3701349d7c6e3
Nahom Debele <nahomdebele002@gmail.com>
2024-02-02 21:21:36 +0300

    added the single note page

--- src/components/NoteComponent/NoteList.jsx
+++ src/components/NoteComponent/NoteList.jsx
@@ -1,22 +1,24 @@
 // components/NoteList.js
 import React from "react";
 import { List, Button, Table } from "antd";
+import { Link } from "react-router-dom";
 
 const NoteList = ({ notes, deleteNote }) => {
   console.log(notes);
   <div className="mt-4">
     {notes.map((note, index) => (
-      <div
+      <Link
+        to={note?._id}
         key={index}
         className="bg-gray-100 p-2 mb-2 flex justify-between items-center"
       >
         <div>{note.text}</div>
         <div>
-          <Button type="danger" onClick={() => deleteNote(index)}>
+          <Button type="danger" onClick={() => deleteNote(note.id)}>
             Delete
           </Button>
         </div>
-      </div>
+      </Link>
     ))}
   </div>;
 };
