caba68f75e6024846a1b4979d2f9b0fddaee36b7
Nahom Debele <nahomdebele002@gmail.com>
2024-02-02 23:05:30 +0300

    added the reporting page

---
+++ src/Pages/Reports.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Dashboard from "../commons/Dashboard";
+
+function Reports() {
+  return (
+    <Dashboard>
+      <div>Reports</div>
+    </Dashboard>
+  );
+}
+
+export default Reports;
