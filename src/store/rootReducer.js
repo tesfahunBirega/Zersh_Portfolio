c9f3bf9d6168b6dc314b3ad23098b0633edc67d1
Nahom Debele <nahomdebele002@gmail.com>
2024-02-06 22:59:08 +0300

    added the catagory redux crud

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -3,13 +3,15 @@ import { blogSlice, blogsSlice } from "./blog/blogSlice";
 import { getNotesSlice } from "./note/noteSlice";
 import { getOkrSlice, getOkrsSlice } from "./okr/okrSlice";
 import { projectSlice } from "./project/projectSlice";
+import { CatagorySlice } from "./catagory/catagorySlice";
 const rootReducer = combineReducers({
   blog:blogSlice.reducer,
   blogs:blogsSlice.reducer,
   notes:getNotesSlice.reducer,
   okr:getOkrSlice.reducer,
   okrs:getOkrsSlice.reducer,
-  project:projectSlice.reducer
+  project:projectSlice.reducer,
+  catagory:CatagorySlice.reducer
 });
 
 export default rootReducer;
