7d103b310235929c7c9ea0c6afd3e9133100a130
Nahom Debele <nahomdebele002@gmail.com>
2024-02-12 23:49:54 +0300

    added a google auth

--- src/store/note/noteAction.js
+++ src/store/note/noteAction.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { baseUrl } from "../../constants";
+import axios from "axios";
 
 export const fetchNote = createAsyncThunk(
     'note/fetchbyid',
