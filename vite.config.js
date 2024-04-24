92be0aacfff5a70a25dd24b121c850e0e962a644
nahom <nahomdebele002@gmail.com>
2024-04-24 21:48:45 +0300

    .env added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
+
+import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'import.meta.env.ENV': JSON.stringify(process.env.ENV),
+    'import.meta.env.BASE_URL_DEV': JSON.stringify(process.env.BASE_URL_DEV),
+    'import.meta.env.BASE_URL_PROD': JSON.stringify(process.env.BASE_URL_PROD)
+  }
+
+});
