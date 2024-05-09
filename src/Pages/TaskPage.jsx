11efcbd053538bc1fe587111757228c19cf5ec9b
nahom <nahomdebele002@gmail.com>
2024-05-09 23:08:47 +0300

    task page fixed

--- src/Pages/TaskPage.jsx
+++ src/Pages/TaskPage.jsx
@@ -1,118 +1,138 @@
 import React, { useState } from 'react';
-import { Checkbox, Input, Button, Modal, Select, List } from 'antd';
+import { Button, List, Card, Typography, Descriptions, Divider } from 'antd';
 import moment from 'moment';
 import Dashboard from '../commons/Dashboard';
+import DisplayTime from '../components/Tasks/displayTime';
+import TimeButton from '../components/Tasks/timeButton';
+import { CurrentDayOfYearCard } from '../components/Tasks/Cards/todayCard';
+import { WastedTimeCard } from '../components/Tasks/Cards/wastedTime';
+import { ProductiveTimeCard } from '../components/Tasks/Cards/ProductiveTimeCard';
+import TaskAddModal from '../components/Tasks/Modals/TaskAdd';
+import useTimerStore from '../store/zustand/timerStore';
+import { ClockCircleOutlined, CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
 
-const { Option } = Select;
+
+const { Text } = Typography;
 
 const TaskManager = () => {
-  const [tasks, setTasks] = useState([
-    { name: 'Task 1', description: 'Description of Task 1', achieved: false, time: moment('06:30', 'HH:mm') },
-    { name: 'Task 2', description: 'Description of Task 2', achieved: true, time: moment('07:00', 'HH:mm') },
-    // Add more tasks as needed
-  ]);
-
-  const [newTaskName, setNewTaskName] = useState('');
-  const [newTaskTime, setNewTaskTime] = useState('');
+  const tasks = useTimerStore((state) => state.tasks);
+  const timers = useTimerStore((state) => state.timers);
+  const startTimer = useTimerStore((state) => state.startTimer);
+  const stopTimer = useTimerStore((state) => state.stopTimer);
+  const pauseTimer = useTimerStore((state) => state.pauseTimer);
+  const completeTask = useTimerStore((state) => state.completeTask);
+  const productiveTime =  useTimerStore((state) => state.productiveTime);
+  const wastedTime = useTimerStore((state) => state.wastedTime);
   const [modalVisible, setModalVisible] = useState(false);
+  const currentDayOfYear = moment().dayOfYear();
 
-  const startDate = moment('2024-01-01'); // Assuming Day 1 starts from January 1, 2024
-
-  const calculateCurrentDay = () => {
-    const currentDate = moment();
-    const daysDiff = currentDate.diff(startDate, 'days') + 1;
-    return daysDiff;
-  };
-
-  const handleCheckboxChange = (index) => {
-    const updatedTasks = [...tasks];
-    updatedTasks[index].achieved = !updatedTasks[index].achieved;
-    setTasks(updatedTasks);
-  };
-
-  const handleAddTask = () => {
-    if (newTaskName && newTaskTime) {
-      const newTask = {
-        name: newTaskName,
-        description: '',
-        achieved: false,
-        time: moment(newTaskTime, 'HH:mm'),
-      };
-      setTasks([...tasks, newTask]);
-      setNewTaskName('');
-      setNewTaskTime('');
-      setModalVisible(false);
-    }
+  const handleStartButtonClick = (taskId, duration) => {
+    startTimer(taskId, duration);
   };
 
   const renderTasks = () => {
     return (
-      <List
+      <>
+    <Divider />
+
+       <List
+        itemLayout="vertical"
+        size="large"
         dataSource={tasks}
-        renderItem={(task, index) => (
-          <List.Item key={index} style={{ width: '100%' }}>
-            <Checkbox checked={task.achieved} onChange={() => handleCheckboxChange(index)} />
-            <div style={{ marginLeft: 16, width: 'calc(100% - 100px)' }}>
-              <h3>{task.name}</h3>
-              <p>{task.description}</p>
-            </div>
-            <div style={{ marginLeft: 'auto', width: '100px', textAlign: 'right' }}>{task.time.format('HH:mm')}</div>
+        renderItem={(task) => (
+          <List.Item
+            key={task.id}
+            extra={
+              <div className="grid justify-center items-center gap-4">
+                <DisplayTime duration={task.duration} />
+                <TimeButton
+                  status={
+                    task.status === 'active' ? 0 :
+                    task.status === 'paused' ? 1:
+                    task.status === 'reset' ? 2:
+                    task.status === 'resume' ? 3:
+                    task.status === 'created' ? 4:
+                    null}
+                  resume={() => startTimer(task.id, timers[task.id])}
+                  reset={() => stopTimer(task.id)}
+                  pause={() => pauseTimer(task.id)}
+                  start={() => handleStartButtonClick(task.id, task.duration)}
+                />
+
+                {
+                   task.status !== 'completed' && (
+                    <Button
+                      onClick={() => completeTask(task.id)}
+                      type="primary"
+                      disabled={task.status !== 'active'}
+                    >
+                      Complete
+                    </Button>
+                  )
+                }
+              </div>
+            }
+          >
+            <List.Item.Meta
+  title={<span>{task.name}</span>}
+  description={
+    <div className="flex justify-start items-center gap-4">
+    <Card title="Number Batches" size="small">
+      <Text>{task.batches} {task.batches > 1 ? 'Batches' : 'Batch'}</Text>
+    </Card>
+    <Card title="Total Duration" size="small">
+      <Text>{task.duration} Seconds</Text>
+    </Card>
+    <Card  title="Productive Time" size="small">
+      <Text>
+        <ClockCircleOutlined /> {task.productiveTime} seconds
+      </Text>
+    </Card>
+    <Card title="Wasted Time" size="small">
+      <Text>
+        <CloseCircleOutlined /> {task.wastedTime} seconds
+      </Text>
+    </Card>
+    <Card title="Countdown Time" size="small">
+      <Text>
+        <CloseCircleOutlined /> {task.countDown} seconds
+      </Text>
+    </Card>
+
+  </div>
+  }
+/>
           </List.Item>
         )}
       />
+    <Divider />
+
+
+      </>
+     
     );
-  };
-
-  const generateTimeOptions = () => {
-    const timeOptions = [];
-    const startTime = moment('05:30', 'HH:mm');
-    const endTime = moment('22:30', 'HH:mm');
-    let currentTime = startTime.clone();
-
-    while (currentTime.isBefore(endTime)) {
-      // Add work period (25 minutes)
-      for (let i = 0; i < 5; i++) {
-        timeOptions.push(
-          <Option key={currentTime.format('HH:mm')} value={currentTime.format('HH:mm')}>
-            {currentTime.format('HH:mm')}
-          </Option>
-        );
-        currentTime.add(5, 'minutes');
-      }
-
-      // Add rest period (5 minutes)
-      currentTime.add(5, 'minutes');
-    }
-    return timeOptions;
   };
 
   return (
     <Dashboard>
-      <h2>Day {calculateCurrentDay()}</h2>
+      <div className="flex justify-start items-center gap-4">
+        <CurrentDayOfYearCard currentDayOfYear={currentDayOfYear} />
+        <ProductiveTimeCard productiveTime={productiveTime} />
+        <WastedTimeCard wastedTime={wastedTime} />
+        <Card title="Card" style={{ width: 300 }} />
+        <Card title="Card" style={{ width: 300 }} />
+      </div>
 
       <div className="flex" style={{ width: '100%' }}>
-
         <div className="m-4" style={{ width: 'calc(100% - 300px)' }}>
-          <Button type="primary" onClick={() => setModalVisible(true)}>Add Task</Button>
-          <Modal
-            title="Add Task"
-            open={modalVisible}
-            onCancel={() => setModalVisible(false)}
-            footer={[
-              <Button key="cancel" onClick={() => setModalVisible(false)}>Cancel</Button>,
-              <Button key="add" type="primary" onClick={handleAddTask}>Add Task</Button>,
-            ]}
-          >
-            <Input placeholder="Task Name" value={newTaskName} onChange={(e) => setNewTaskName(e.target.value)} />
-            <Select
-              placeholder="Select Time"
-              style={{ width: '100%', marginTop: 10 }}
-              value={newTaskTime}
-              onChange={(value) => setNewTaskTime(value)}
-            >
-              {generateTimeOptions()}
-            </Select>
-          </Modal>
+          <Button type="primary" onClick={() => setModalVisible(true)}>
+            Add Task
+          </Button>
+          <TaskAddModal
+            setModalVisible={setModalVisible}
+            modalVisible={modalVisible}
+            
+          />
           {renderTasks()}
         </div>
       </div>
