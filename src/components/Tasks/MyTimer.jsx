78c575d8b70da95bd448b9e3d8b6b4d239782da0
Nahi002 <nahomdebele002@gmail.com>
2024-06-03 22:35:49 +0300

    Added my timer

---
+++ src/components/Tasks/MyTimer.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useTimer, useStopwatch } from 'react-timer-hook';
+
+export function MyTimer({ expiryTimestamp, onExpire }) {
+  const {
+    totalSeconds,
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    resume,
+    restart,
+  } = useTimer({ expiryTimestamp, onExpire });
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <div style={{ fontSize: '50px' }}>
+        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+      </div>
+      <p>{isRunning ? 'Running' : 'Not running'}</p>
+      <button onClick={start}>Start</button>
+      <button onClick={pause}>Pause</button>
+      <button onClick={resume}>Resume</button>
+      <button onClick={() => {
+        // Restarts to 5 minutes timer
+        const time = new Date();
+        time.setSeconds(time.getSeconds() + 300);
+        restart(time)
+      }}>Restart</button>
+    </div>
+  );
+}
+
+export function MyStopwatch() {
+  const {
+    totalSeconds,
+    seconds,
+    minutes,
+    hours,
+    days,
+    isRunning,
+    start,
+    pause,
+    reset,
+  } = useStopwatch({ autoStart: true });
+
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <div style={{ fontSize: '50px' }}>
+        <span>{days}</span>:<span>{hours}</span>:<span>{minutes}</span>:<span>{seconds}</span>
+      </div>
+      <p>{isRunning ? 'Running' : 'Not running'}</p>
+      <button onClick={start}>Start</button>
+      <button onClick={pause}>Pause</button>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+}
